2877d21ee453a77c91085c7b653fadfe348517d2
art.grabovski <art.grabovski@gmail.com>
2025-05-16 15:04:56 +0200

    added RouteConstants

--- src/Finturest.Email/Options/EmailOptions.cs
+++ src/Finturest.Email/Options/EmailOptions.cs
@@ -4,5 +4,5 @@ public record EmailOptions
 {
     public string ApiKey { get; set; }
 
-    public string BaseAddress { get; set; }
+    public string BaseAddress { get; set; } = "https://api.finturest.com";
 }
