7eaa2de671346d950d508ffa8b4e38d84f18970f
art.grabovski <art.grabovski@gmail.com>
2025-05-16 15:55:42 +0200

    Added assertion

--- tests/Finturest.Email.IntegrationTests/EmailServiceClientIntegrationTests.cs
+++ tests/Finturest.Email.IntegrationTests/EmailServiceClientIntegrationTests.cs
@@ -1,6 +1,7 @@
 ﻿using Finturest.Email.DependencyInjection;
 
 using Fitnurest.Email.Abstractions;
+using Fitnurest.Email.Abstractions.Models.Enums;
 using Fitnurest.Email.Abstractions.Models.Requests;
 
 using Microsoft.Extensions.DependencyInjection;
@@ -44,5 +45,20 @@ public class EmailServiceClientIntegrationTests
 
         // Assert
         result.ShouldNotBeNull();
+
+        result.Deliverable.ShouldBe(true);
+
+        result.Validations.Count.ShouldBe(2);
+
+        result.Validations[0].Type.ShouldBe(EmailValidationType.Format);
+        result.Validations[0].Status.ShouldBe(EmailValidationStatus.Passed);
+
+        result.Validations[1].Type.ShouldBe(EmailValidationType.Mx);
+        result.Validations[1].Status.ShouldBe(EmailValidationStatus.Passed);
+
+        result.Classifications.ShouldNotBeNull();
+        result.Classifications.Disposable.ShouldBeFalse();
+        result.Classifications.Free.ShouldBeFalse();
+        result.Classifications.RoleBased.ShouldBeTrue();
     }
 }
