83789d12385769d3cc198cb81d5a277474504f44
art.grabovski <art.grabovski@gmail.com>
2025-05-16 15:44:10 +0200

    Added documentation

--- src/Fitnurest.Email.Abstractions/IEmailServiceClient.cs
+++ src/Fitnurest.Email.Abstractions/IEmailServiceClient.cs
@@ -3,7 +3,17 @@ using Fitnurest.Email.Abstractions.Models.Responses;
 
 namespace Fitnurest.Email.Abstractions;
 
+/// <summary>
+/// Provides methods for sending requests to and receiving responses from the Finturest Email API.
+/// </summary>
 public interface IEmailServiceClient
 {
+    /// <summary>
+    /// Validate email
+    /// </summary>
+    /// <param name="request">Request model to validate email</param>
+    /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+    /// <exception cref="ArgumentNullException">The request model was null.</exception>
+    /// <exception cref="HttpRequestException">The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.</exception>
     Task<ValidateEmailResponse> ValidateEmailAsync(ValidateEmailRequest request, CancellationToken cancellationToken = default);
 }
