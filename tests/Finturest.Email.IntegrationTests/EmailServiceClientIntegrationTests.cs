32b3caf54ae3d9e6b33ecac110c874568e2dfd2d
art.grabovski <art.grabovski@gmail.com>
2025-05-16 16:00:30 +0200

    Added more test cases

--- tests/Finturest.Email.IntegrationTests/EmailServiceClientIntegrationTests.cs
+++ tests/Finturest.Email.IntegrationTests/EmailServiceClientIntegrationTests.cs
@@ -30,7 +30,7 @@ public class EmailServiceClientIntegrationTests
     }
 
     [Fact]
-    public async Task ValidateEmailAsync_EmailIsValid_EnsureSuccessStatusCocde()
+    public async Task ValidateEmailAsync_EmailIsValid_ReturnCorrectResult()
     {
         // Arrange
         var request = new ValidateEmailRequest
@@ -58,5 +58,58 @@ public class EmailServiceClientIntegrationTests
         result.Classifications.Disposable.ShouldBeFalse();
         result.Classifications.Free.ShouldBeFalse();
         result.Classifications.RoleBased.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task ValidateEmailAsync_EmailHasFormatErrors_ReturnCorrectResult()
+    {
+        // Arrange
+        var request = new ValidateEmailRequest
+        {
+            Email = "support@@finturest.com"
+        };
+
+        // Act
+        var result = await _sut.ValidateEmailAsync(request);
+
+        // Assert
+        result.ShouldNotBeNull();
+
+        result.Deliverable.ShouldBe(false);
+
+        result.Validations.Count.ShouldBe(1);
+
+        result.Validations[0].Type.ShouldBe(EmailValidationType.Format);
+        result.Validations[0].Status.ShouldBe(EmailValidationStatus.Failed);
+
+        result.Classifications.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task ValidateEmailAsync_EmailHasInvalidDomain_ReturnCorrectResult()
+    {
+        // Arrange
+        var request = new ValidateEmailRequest
+        {
+            Email = "support@test.com"
+        };
+
+        // Act
+        var result = await _sut.ValidateEmailAsync(request);
+
+        // Assert
+        result.ShouldNotBeNull();
+
+        result.Deliverable.ShouldBe(false);
+
+        result.Validations.Count.ShouldBe(2);
+
+        result.Validations[0].Type.ShouldBe(EmailValidationType.Format);
+        result.Validations[0].Status.ShouldBe(EmailValidationStatus.Passed);
+
+        result.Validations[1].Type.ShouldBe(EmailValidationType.Mx);
+        result.Validations[1].Status.ShouldBe(EmailValidationStatus.Failed);
+
+        result.Classifications.ShouldBeNull();
     }
 }
