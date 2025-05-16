0cf7b63f2deb20e6b021cc17c621b4c79954959a
art.grabovski <art.grabovski@gmail.com>
2025-05-16 15:31:45 +0200

    Added EmailServiceClientIntegrationTests

---
+++ tests/Finturest.Email.IntegrationTests/EmailServiceClientIntegrationTests.cs
@@ -0,0 +1,48 @@
+﻿using Finturest.Email.DependencyInjection;
+
+using Fitnurest.Email.Abstractions;
+using Fitnurest.Email.Abstractions.Models.Requests;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Shouldly;
+
+namespace Finturest.Email.IntegrationTests;
+
+/// <summary>
+/// Integration tests for <see cref="IEmailServiceClient"/>
+/// </summary>
+public class EmailServiceClientIntegrationTests
+{
+    private readonly IEmailServiceClient _sut;
+
+    public EmailServiceClientIntegrationTests()
+    {
+        var services = new ServiceCollection();
+
+        services.AddFinturestEmail(options =>
+        {
+            options.ApiKey = "hidden";
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        _sut = serviceProvider.GetRequiredService<IEmailServiceClient>();
+    }
+
+    [Fact]
+    public async Task ValidateEmailAsync_EmailIsValid_EnsureSuccessStatusCocde()
+    {
+        // Arrange
+        var request = new ValidateEmailRequest
+        {
+            Email = "support@finturest.com"
+        };
+
+        // Act
+        var result = await _sut.ValidateEmailAsync(request);
+
+        // Assert
+        result.ShouldNotBeNull();
+    }
+}
