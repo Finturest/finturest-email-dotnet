a01581cce2270643e2cd81a08db5c0ef944fb5b0
art.grabovski <art.grabovski@gmail.com>
2025-05-17 16:58:54 +0200

    Update docs

--- src/Finturest.Email/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Finturest.Email/DependencyInjection/ServiceCollectionExtensions.cs
@@ -9,8 +9,18 @@ using Microsoft.Extensions.Options;
 
 namespace Finturest.Email.DependencyInjection;
 
+/// <summary>
+/// Provides extension methods to register the Finturest Email API client and its dependencies with the dependency injection container.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds and configures the Finturest Email client using configuration from the specified <see cref="IConfigurationSection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="configurationSection">The configuration section containing settings for <see cref="EmailOptions"/>.</param>
+    /// <returns>The same <see cref="IServiceCollection"/> instance so that additional calls can be chained.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="services"/> or <paramref name="configurationSection"/> is <c>null</c>.</exception>
     public static IServiceCollection AddFinturestEmail(this IServiceCollection services, IConfigurationSection configurationSection)
     {
 #if NET6_0_OR_GREATER
@@ -35,6 +45,13 @@ public static class ServiceCollectionExtensions
         return services;
     }
 
+    /// <summary>
+    /// Adds and configures the Finturest Email client using an action delegate to set <see cref="EmailOptions"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="configureOptions">An action delegate to configure the <see cref="EmailOptions"/>.</param>
+    /// <returns>The same <see cref="IServiceCollection"/> instance so that additional calls can be chained.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="services"/> or <paramref name="configureOptions"/> is <c>null</c>.</exception>
     public static IServiceCollection AddFinturestEmail(this IServiceCollection services, Action<EmailOptions> configureOptions)
     {
 #if NET6_0_OR_GREATER
